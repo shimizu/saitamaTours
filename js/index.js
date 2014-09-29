9b510ea36533872264546600a2a5abf8b2eb2022
shimizu masayuki <xxshimizuxx@gmail.com>
2014-09-29 15:12:00 +0900

    盆栽村ルートを追加

--- js/index.js
+++ js/index.js
@@ -1,28 +1,99 @@
-d3.json('./_data/saitama.geojson?1', function(pointjson){
-	main(pointjson); 
-});
+/*後でまとめてリファクタリングする*/
 
-function main(pointjson) {
- 
-	var  style_array_from_above_here = [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#50a5d1"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"gamma":1},{"saturation":50}]}];   
+var snapper;
+var overlay = new google.maps.OverlayView();
+var svgoverlay
+var tourElemnt;
+var googleMapProjection;
+var path;
+var map;
 
-	//Google Map 初期化
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 16,
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		styles: style_array_from_above_here,
-		center: { lat: 35.906961392699998, lng: 139.622833797 },       
+
+/*初期化*/
+snapper_init();
+webix_init();
+map = $$("map").map.data.map;
+overlay_init();
+
+/*起動時の読み込み*/
+setTimeout(function(){
+	drawTour("./_data/tetsudou.json?1");
+}, 1000);
+
+/*ルート選択(スライドメニュー)選択時のイベント*/
+d3.selectAll(".tour").on("click", function(){
+	console.log(this.dataset.geojsonFile);
+	tourElemnt.remove();
+	tourElemnt = svgoverlay.append("g");
+	drawTour(this.dataset.geojsonFile);
+	var latlng = this.dataset.startPoint.split(",");
+	map.panTo(new google.maps.LatLng(latlng[0], latlng[1]));
+	snapper.close();
+})
+
+
+function snapper_init(){
+	snapper = new Snap({
+		element: document.getElementById('snap_content')
+	});	
+	
+	var addEvent = function addEvent(element, eventName, func) {
+		if (element.addEventListener) {
+			return element.addEventListener(eventName, func, false);
+		} else if (element.attachEvent) {
+			return element.attachEvent("on" + eventName, func);
+		}
+	};
+	
+	addEvent(document.getElementById('open-left'), 'click', function(){
+		if( snapper.state().state=="left" ){
+			snapper.close();
+		} else {
+			snapper.open('left');
+		}
 	});
+}	
 
-		
-	var overlay = new google.maps.OverlayView(); //OverLayオブジェクトの作成
+function webix_init(){
+	webix.ui.fullScreen();
+	var gmap_View = {
+		id:"map",
+		view:"google-map",
+		zoom:16,
+		center:[ 35.906961392699998, 139.622833797 ]
+	}
+	
+	var detail_view = {
+		id:"detail",	
+		rows:[
+			{ view:"button", id:"back_btn", value:"地図に戻る" },
+			{
+				template:'<div id="detail_content"></div>'
+			}
+		]
+	}
+	
+	webix.ui({
+		container:"main_view",
+		animate:{type:"flip"},
+		cells:[gmap_View, detail_view],
+	});
+	
+	$$("back_btn").attachEvent("onItemClick", function(){
+		$$("map").show();
+	});
+}
 
+
+function overlay_init(){
+	
 	//オーバレイ追加
 	overlay.onAdd = function () {
 
-		var layer = d3.select(this.getPanes().floatPane).append("div").attr("class", "SvgOverlay");
+		var layer = d3.select(this.getPanes().overlayMouseTarget).append("div").attr("class", "SvgOverlay");
 		var svg = layer.append("svg");
-		var svgoverlay = svg.append("g").attr("class", "AdminDivisions");
+		svgoverlay = svg.append("g").attr("class", "AdminDivisions");
+		tourElemnt = svgoverlay.append("g")
 		var defs = svg.append("defs");
 		var clipPath = defs.append("clipPath").attr("id", "photoClip");
 		clipPath.append("circle").attr({
@@ -35,16 +106,27 @@ function main(pointjson) {
 		var overlayProjection = markerOverlay.getProjection();
 
 		//Google Mapの投影法設定
-		var googleMapProjection = function (coordinates) {
+		googleMapProjection = function (coordinates) {
 			var googleCoordinates = new google.maps.LatLng(coordinates[1], coordinates[0]);
 			var pixelCoordinates = overlayProjection.fromLatLngToDivPixel(googleCoordinates);
 			return [pixelCoordinates.x + 4000, pixelCoordinates.y + 4000];
 		}
 		
 		//パスジェネレーター作成
-        path = d3.geo.path().projection(googleMapProjection);　
-			
-			
+        path = d3.geo.path().projection(googleMapProjection);
+		
+		overlay.draw = function(){};
+	}
+	
+	//作成したSVGを地図にオーバーレイする
+	overlay.setMap(map);
+	
+		
+}
+
+function drawTour(geojsonFIle){
+	
+	d3.json(geojsonFIle, function(pointjson){
 		//再描画時に呼ばれるコールバック    
 		overlay.draw = function () {
 
@@ -54,9 +136,13 @@ function main(pointjson) {
 			//ピクセルポジション情報
 			var positions = [];
 			 points = {"type": "LineString", "coordinates": []};
+			 
+			if(pointdata.length <= 0) return null;
 
-			pointdata.forEach(function(d) {		
-				positions.push(googleMapProjection(d.geometry.coordinates));
+			pointdata.forEach(function(d) {
+				var b= d.geometry.coordinates;
+				var a = googleMapProjection(b)
+				positions.push(a);
 				points.coordinates.push([d.geometry.coordinates[0], d.geometry.coordinates[1]]);
 			});
 			
@@ -69,7 +155,7 @@ function main(pointjson) {
 						"stroke": "green",
 						"stroke-width": 6
 					}
-				var line = svgoverlay.selectAll(".line")
+				var line = tourElemnt.selectAll(".line")
 					.data([points])
 					.attr(lineAttr)	
 					.enter()
@@ -86,7 +172,7 @@ function main(pointjson) {
 				
 			}
 	
-			var g =svgoverlay.selectAll(".marker")
+			var g =tourElemnt.selectAll(".marker")
 				.data(pointdata)
 				.attr(gAttr)
 				.enter()
@@ -108,7 +194,12 @@ function main(pointjson) {
 				g.append("image")
 				.attr({
 					"xlink:href":function(d){
-						var img = d.properties['pdfmaps_photos'].match(/(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/)[0];
+						if (d.properties['pdfmaps_photos'] !="") {
+							console.log("img",d.properties['pdfmaps_photos']);
+							var img = d.properties['pdfmaps_photos'].match(/(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/)[0];
+						}else{
+							var img = "img/noImage_s.jpg";
+						}
 						return img;
 					},
 					x:-60,
@@ -116,15 +207,36 @@ function main(pointjson) {
 					"width":120,
 					"height":120,
 					"clip-path":"url(#photoClip)",
+				})
+				.on("click", function(d){
+					$$("detail").show();
+					data = d.properties;
+					/*todo:↓後で直す*/
+					var html = "<center>";
+					html += data["Name"];
+					html += data["06_メモ"];
+					html += "<br>";
+					html += data["pdfmaps_photos"];
+					document.querySelector("#detail_content").innerHTML = html;
 				});
 
-	  
 		};
-
-	};
-
-	//作成したSVGを地図にオーバーレイする
-	overlay.setMap(map);
-	
 		
-};
+		//再描画
+		overlay.draw();		
+	});
+
+}
+
+/* モバイルsafariがリンクを開くのを防ぐ */
+(function (a, b, c) {
+    if(c in b && b[c]) {
+        var d, e = a.location,
+            f = /^(a|html)$/i;
+        a.addEventListener("click", function (a) {
+            d = a.target;
+            while(!f.test(d.nodeName)) d = d.parentNode;
+            "href" in d && (d.href.indexOf("http") || ~d.href.indexOf(e.host)) && (a.preventDefault(), e.href = d.href)
+        }, !1)
+    }
+})(document, window.navigator, "standalone");
